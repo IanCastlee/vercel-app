4b50873bef2c0fc57cf1333b186bd3aeeb30aa17
Eyhannn <castlegithub08@gmail.com>
2024-03-26 21:41:03 +0800

    v3

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -7,8 +6,8 @@ import registerRoutes from "./routes/register.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true // Allow sending cookies from frontend
+  origin: "https://vercel-app-client-ten.vercel.app",
+  credentials: true // Pahintulutan ang pagpapadala ng cookies mula sa frontend
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -16,5 +15,5 @@ app.use(cookieParser());
 app.use("/api/register", registerRoutes);
 
 app.listen(8801, () => {
-  console.log("API working in 8801");
+  console.log("API gumagana sa 8801");
 });
